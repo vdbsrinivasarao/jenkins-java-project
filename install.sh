594f911f015ae606638f89a5c0312c77768ced3f
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2024-12-15 21:10:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-aws s3 cp s3://vdbjavawebapp/myjavaproject/NETFLIX-1.2.2.war 
+aws s3 cp s3://vdbjavawebapp/myjavaproject/NETFLIX-1.2.2.war /tmp
 sudo mv /tmp/NETFLIX-1.2.2.war   /root/apache-tomcat-9.0.97/webapps
 sudo service tomcat restart
