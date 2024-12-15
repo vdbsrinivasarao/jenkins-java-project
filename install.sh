96a5c75f4de83e7b057235f704c6aa142eb9a38f
vdbsrinivasarao <113365336+vdbsrinivasarao@users.noreply.github.com>
2024-12-15 20:46:38 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-aws s3 cp s3://vdbjavawebapp/myjavaproject/myjavawebapp.war
-sudo mv /tmp/myjavawebapp.war  /root/apache-tomcat-9.0.97/webapps
+aws s3 cp s3://vdbjavawebapp/myjavaproject/myjavaproject.war
+sudo mv /tmp/myjavaproject.war  /root/apache-tomcat-9.0.97/webapps
 sudo service tomcat restart
